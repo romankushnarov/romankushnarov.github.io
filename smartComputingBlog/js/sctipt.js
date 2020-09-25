23824d2fd79b145edaaa2ed21b22794c98ef97eb
Roman Kushnarov <kushnarov.roman@@gmail.com>
2020-09-26 02:57:55 +0300

    fixed header

--- smartComputingBlog/js/sctipt.js
+++ smartComputingBlog/js/sctipt.js
@@ -1,11 +1,16 @@
-const menuToggle = document.querySelector('#menuToggle');
-const mobileMenuToggle = document.querySelector('#mobileMenuToggle');
+$(document).ready(function(){
 
-menuToggle.onclick = function(){
-    document.querySelector('.mobile-line').classList.toggle('mobile-line-active');
-    document.querySelector('.mobile-nav').classList.toggle('mobile-nav-active');
-}
+    const menuToggle = document.querySelector('#menuToggle');
+    const mobileMenuToggle = document.querySelector('#mobileMenuToggle');
+    
+    menuToggle.onclick = function(){
+        document.querySelector('.mobile-line').classList.toggle('mobile-line-active');
+        document.querySelector('.mobile-nav').classList.toggle('mobile-nav-active');
+    }
 
-$(".mob-search").click(function(){ // задаем функцию при нажатиии на элемент <button>
-  $( ".mob-search-form" ).focus(); // вызываем событие focus на элементе <div>
+  $(".fa-search").click(function(){
+    $(".mob-search-container, .mob-search-input").toggleClass("active");
+    $(".mob-search-input").focus();
+  });
+  
 });
