7ddd46279fecfe0b127d7d9a0be67ddf5cc59a08
Roman Kushnarov <kushnarov.roman@@gmail.com>
2020-09-29 18:49:45 +0300

    fixed mobile smartcompblog

--- smartComputingBlog/js/sctipt.js
+++ smartComputingBlog/js/sctipt.js
@@ -14,24 +14,3 @@ mobileMenuToggleClose.onclick = function() {
   document.querySelector('.mobile-line').classList.remove('mobile-line-active');
   document.querySelector('.mobile-nav').classList.remove('mobile-nav-active');
 }
-
-let counterMenu = 0;
-document.querySelector(".mob-search_toggler").onclick = function(){
-
-  if(counterMenu % 2 == 0) {
-    document.querySelector('.mob-search_toggler-btn').classList.remove('fa-search');
-    document.querySelector('.mob-search_toggler-btn').classList.add('fa-times');
-    counterMenu++;
-  } else {
-    document.querySelector('.mob-search_toggler-btn').classList.remove('fa-times');
-    document.querySelector('.mob-search_toggler-btn').classList.add('fa-search');
-    counterMenu++;
-  }
-
-  
-
-  document.querySelector(".mob-search-container").classList.toggle("active");
-  document.querySelector(".mob-search-input").classList.toggle("active");
-  document.querySelector(".mob-search-input").focus();
-  
-};
