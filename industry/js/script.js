71e732cda6f6894acc564c7c6a810d6bfbd7a516
Roman Kushnarov <kushnarov.roman@@gmail.com>
2020-10-03 19:53:27 +0300

    added new project industry

---
+++ industry/js/script.js
@@ -0,0 +1,33 @@
+const menuToggle = document.querySelector('#menuToggle');
+const mobileMenuToggle = document.querySelector('#mobileMenuToggle');
+
+let counter = 0; 
+menuToggle.onclick = function(){
+    document.querySelector('.mobile-line').classList.toggle('mobile-line-active');
+    document.querySelector('.mobile-nav').classList.toggle('mobile-nav-active');
+    if(counter % 2 == 0) {
+        document.querySelector('body').style.overflow = 'hidden';
+        counter ++;
+    } else {
+        document.querySelector('body').style.overflow = 'auto';
+        counter++;
+    }
+}
+
+window.onscroll = function() {
+   var scrollElem = document.getElementById("scrollToTop");
+   if (document.documentElement.scrollTop > 1000) {
+      scrollElem.style.opacity = "1";
+   } else {
+       scrollElem.style.opacity = "0";
+   }
+}
+
+var timeOut;
+function goUp() {
+   var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+   if(top > 0) {
+      window.scrollBy(0,-100);
+      timeOut = setTimeout('goUp()',20);
+   } else clearTimeout(timeOut);
+}
--- smartComputingBlog/js/sctipt.js
+++ smartComputingBlog/js/sctipt.js
@@ -6,11 +6,26 @@ const mobileMenuToggleClose = document.querySelector("#mobile-nav_cross")
 
 
 menuToggle.onclick = function(){
-  document.querySelector('.mobile-line').classList.add('mobile-line-active');
-  document.querySelector('.mobile-nav').classList.add('mobile-nav-active');
+  document.querySelector('.mobile-line').classList.toggle('block');
+  document.querySelector('.mobile-nav').classList.toggle('block');
+  document.querySelector('.mobile-line').classList.toggle('mobile-line-active');
+  document.querySelector('.mobile-nav').classList.toggle('mobile-nav-active');
 }
 
-mobileMenuToggleClose.onclick = function() {
-  document.querySelector('.mobile-line').classList.remove('mobile-line-active');
-  document.querySelector('.mobile-nav').classList.remove('mobile-nav-active');
+let counterMenu = 0;
+document.querySelector('.mob-search_toggler-btn').onclick = function() {
+  document.querySelector('.mob-search-input').classList.toggle('active');
+  if(counterMenu % 2 == 0) {
+    document.querySelector('.mob-search_toggler-btn').classList.remove('fa-search');
+    document.querySelector('.mob-search_toggler-btn').classList.add('fa-times');
+    document.querySelector('.form-btn').style.display = 'block';
+    counterMenu++;
+  } else {
+    document.querySelector('.mob-search_toggler-btn').classList.remove('fa-times');
+    document.querySelector('.mob-search_toggler-btn').classList.add('fa-search');
+    document.querySelector('.form-btn').style.display = 'none';
+    counterMenu++;
+  }
+
+  
 }
