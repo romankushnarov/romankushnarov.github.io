3b9210bdcd87d208887bc1f42614a1557814ea70
Roman Kushnarov <kushnarov.roman@@gmail.com>
2020-10-16 01:00:27 +0300

    new gomeovet

---
+++ gomeovet2/js/script.js
@@ -0,0 +1,55 @@
+$('.extra-shelter__carousel').owlCarousel({
+  loop:true,
+  responsiveClass:true,
+  items: 1,
+  nav: true,
+  navSpeed: 1000,
+  navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+})
+
+$('.home__carousel').owlCarousel({
+  loop:true,
+  responsiveClass:true,
+  nav: true,
+  navSpeed: 1000,
+  navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+  responsive:{
+    0:{
+      items:1,
+    },
+    800:{
+      items:2,
+    }
+}
+})
+
+$('.reports-carousel').owlCarousel({
+  loop:true,
+  responsiveClass:true,
+  nav: true,
+  navSpeed: 1000,
+  navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+  responsive:{
+    0:{
+      items:1,
+    },
+    768:{
+      items:2,
+    },
+    1000:{
+      items:3,
+    },
+    1280:{
+      items:4,
+    }
+}
+})
+
+
+// ---------------- FAQ ----------------
+
+$('.question-text').hide();
+$('.question').on('click', function() {
+    $(this).find('.question-text').slideToggle(500);
+    console.log($(this));
+});
