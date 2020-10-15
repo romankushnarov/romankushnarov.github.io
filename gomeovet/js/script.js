3ff3b53fb5a10c3b5f3cc6e65879452ea4ef87a8
Roman Kushnarov <kushnarov.roman@@gmail.com>
2020-10-15 22:51:49 +0300

    gomeovet before removing bg

--- gomeovet/js/script.js
+++ gomeovet/js/script.js
@@ -1,10 +1,46 @@
-
 $('.extra-shelter__carousel').owlCarousel({
   loop:true,
-//   margin:10,
   responsiveClass:true,
   items: 1,
   nav: true,
   navSpeed: 1000,
   navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+})
+
+$('.home__carousel').owlCarousel({
+  loop:true,
+  responsiveClass:true,
+  nav: true,
+  navSpeed: 1000,
+  navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+  responsive:{
+    0:{
+      items:1,
+    },
+    800:{
+      items:2,
+    }
+}
+})
+
+$('.reports-carousel').owlCarousel({
+  loop:true,
+  responsiveClass:true,
+  nav: true,
+  navSpeed: 1000,
+  navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+  responsive:{
+    0:{
+      items:1,
+    },
+    768:{
+      items:2,
+    },
+    1000:{
+      items:3,
+    },
+    1280:{
+      items:4,
+    }
+}
 })
