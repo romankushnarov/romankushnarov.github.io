84e8f825f3902dfe17c950d3dc3ba741293a7315
Roman Kushnarov <kushnarov.roman@@gmail.com>
2020-10-15 13:23:24 +0300

    slider 1 almost ready

---
+++ gomeovet/js/script.js
@@ -0,0 +1,10 @@
+
+$('.extra-shelter__carousel').owlCarousel({
+  loop:true,
+//   margin:10,
+  responsiveClass:true,
+  items: 1,
+  nav: true,
+  navSpeed: 1000,
+  navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+})
