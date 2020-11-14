b520d72107955a654e2dad35788731a34b477b8d
Roman Kushnarov <kushnarov.roman@@gmail.com>
2020-11-14 21:39:00 +0300

    second try fix scroll to anchor

--- abus/js/script.js
+++ abus/js/script.js
@@ -1,36 +1,38 @@
-const menuToggle = document.querySelector('#menuToggle');
-const mobileMenuToggle = document.querySelector('#mobileMenuToggle');
+$(document).ready(function() {
+    const menuToggle = document.querySelector('#menuToggle');
+    const mobileMenuToggle = document.querySelector('#mobileMenuToggle');
 
-menuToggle.onclick = function(){
-    document.querySelector('.mobile-line').classList.toggle('mobile-line-active');
-    document.querySelector('.mobile-nav').classList.toggle('mobile-nav-active');
-    document.querySelector('.overlay').classList.toggle('block');
-    document.querySelector('body').classList.toggle('hidden');
-}
+    menuToggle.onclick = function(){
+        document.querySelector('.mobile-line').classList.toggle('mobile-line-active');
+        document.querySelector('.mobile-nav').classList.toggle('mobile-nav-active');
+        document.querySelector('.overlay').classList.toggle('block');
+        document.querySelector('body').classList.toggle('hidden');
+    }
 
-document.querySelector('.mobile-nav__list').onclick = function() {
-    document.querySelector('.mobile-line').classList.toggle('mobile-line-active');
-    document.querySelector('.mobile-nav').classList.toggle('mobile-nav-active');
-    document.querySelector('.overlay').classList.toggle('block');
-    document.querySelector('body').classList.toggle('hidden');
-}
+    document.querySelector('.mobile-nav__list').onclick = function() {
+        document.querySelector('.mobile-line').classList.toggle('mobile-line-active');
+        document.querySelector('.mobile-nav').classList.toggle('mobile-nav-active');
+        document.querySelector('.overlay').classList.toggle('block');
+        document.querySelector('body').classList.toggle('hidden');
+    }
 
 
-document.querySelector('.overlay').onclick = function() {
-    document.querySelector('.mobile-line').classList.toggle('mobile-line-active');
-    document.querySelector('.mobile-nav').classList.toggle('mobile-nav-active');
-    document.querySelector('.overlay').classList.toggle('block');
-    document.querySelector('body').classList.toggle('hidden');
-}
+    document.querySelector('.overlay').onclick = function() {
+        document.querySelector('.mobile-line').classList.toggle('mobile-line-active');
+        document.querySelector('.mobile-nav').classList.toggle('mobile-nav-active');
+        document.querySelector('.overlay').classList.toggle('block');
+        document.querySelector('body').classList.toggle('hidden');
+    }
 
-// $('shopModal1').ready(function() {
-//     function scrollTo(hash) {
-//         location.hash = "#" + hash;
-//     }
-// });
+    // $('shopModal1').ready(function() {
+    //     function scrollTo(hash) {
+    //         location.hash = "#" + hash;
+    //     }
+    // });
 
-document.querySelector('#shop__item-info__link1').addEventListener('click', function() {
-        setTimeout(() => {
-            document.querySelector('#compare-1').scrollIntoView({behavior: "smooth"}); 
-        }, 500);
+    document.querySelector('#shop__item-info__link1').addEventListener('click', function() {
+            setTimeout(() => {
+                document.querySelector('#compare-1').scrollIntoView({behavior: "smooth"}); 
+            }, 500);
+    });
 });
